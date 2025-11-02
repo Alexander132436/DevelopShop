0ddeaed964ff9879a7d959d081133f4ae0dce91a
Alexander <sanya01-03@mail.ru>
2025-11-02 11:15:56 +0300

    Initial commit: React + ASP.NET Core project structure

---
+++ backend/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/", () =>{});
+
+
+app.Run();
